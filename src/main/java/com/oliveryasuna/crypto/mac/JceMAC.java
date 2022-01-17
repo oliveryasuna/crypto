c5d2465b5e8e954eb8c20180ee7ebc20364965ba
Oliver Yasuna <oliveryasuna.acct@gmail.com>
2022-01-17 13:29:38 -0500

    Created JceMAC.

---
+++ src/main/java/com/oliveryasuna/crypto/mac/JceMAC.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright 2022 Oliver Yasuna
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without
+ *      specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+ * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.oliveryasuna.crypto.mac;
+
+import com.oliveryasuna.commons.language.Arguments;
+import com.oliveryasuna.commons.language.marker.Immutable;
+
+import javax.crypto.Mac;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.security.Provider;
+import java.util.Arrays;
+
+/**
+ * Wraps {@link Mac}.
+ *
+ * @author Oliver Yasuna.
+ */
+@Immutable
+public class JceMAC implements IMAC {
+
+  // Constructors
+  //--------------------------------------------------
+
+  public JceMAC(final Key key, final String algorithm, final Provider provider) throws NoSuchAlgorithmException, InvalidKeyException {
+    super();
+
+    Arguments.requireNotNull(key, "key");
+    Arguments.requireNotNull(algorithm, "algorithm");
+    Arguments.requireNotNull(provider, "provider");
+
+    this.key = key;
+    this.mac = Mac.getInstance(algorithm, provider);
+
+    initMac();
+  }
+
+  public JceMAC(final Key key, final String algorithm) throws NoSuchAlgorithmException, InvalidKeyException {
+    super();
+
+    Arguments.requireNotNull(key, "key");
+    Arguments.requireNotNull(algorithm, "algorithm");
+
+    this.key = key;
+    this.mac = Mac.getInstance(algorithm);
+
+    initMac();
+  }
+
+  // Constructor helper methods
+  //--------------------------------------------------
+
+  private void initMac() throws InvalidKeyException {
+    mac.init(key);
+  }
+
+  // Fields
+  //--------------------------------------------------
+
+  private final Key key;
+
+  private final Mac mac;
+
+  // IMAC methods
+  //--------------------------------------------------
+
+  @Override
+  public byte[] sign(final byte[] message) {
+    Arguments.requireNotNull(message, "message");
+
+    return mac.doFinal(message);
+  }
+
+  @Override
+  public boolean verify(final byte[] message, final byte[] tag) {
+    Arguments.requireNotNull(message, "message");
+    Arguments.requireNotNull(tag, "tag");
+
+    final byte[] expectedTag = sign(message);
+
+    return Arrays.equals(expectedTag, tag);
+  }
+
+  // Getters
+  //--------------------------------------------------
+
+  public Key getKey() {
+    return key;
+  }
+
+  public String getAlgorithm() {
+    return mac.getAlgorithm();
+  }
+
+  public int getMacLength() {
+    return mac.getMacLength();
+  }
+
+  // Object methods
+  //--------------------------------------------------
+
+}
